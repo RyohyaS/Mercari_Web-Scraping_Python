5345be14119f5d343bac2a0511e05ed5f76d8007
forgottencow <forgottencow77@gmail.com>
2024-10-19 00:55:30 +0900

    complete
    pleases translate to eng

--- mercari.py
+++ mercari.py
@@ -4,6 +4,7 @@ import chromedriver_binary
 from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
 
 item_ls = []
 item_url_ls = []
@@ -20,23 +21,37 @@ browser = webdriver.Chrome(options=options)
 browser.implicitly_wait(3)
 
 # キーワード設定
-KEYWORD = "macbook pro 13inch i5 16gb 256gb 2020"
+KEYWORD = ('MacBook Pro i5 2020')
 MAX_ITEMS = 1000
 
 def get_url():
-    # 売り切れのみ表示&除外キーワード&ページトークン
     url = 'https://jp.mercari.com/search?keyword=' + KEYWORD + '&status=sold_out%7Ctrading&exclude_keyword=2012%2C%202013%2C%202014%2C%202015%2C%202016%2C%202017%2C%202018&page_token=v1%3A2'
     browser.get(url)
     browser.implicitly_wait(5)
 
-    # 商品の詳細ページのURLを取得する
-    item_box = browser.find_elements(By.CSS_SELECTOR, '#item-grid > ul > li')
+    # すべてのアイテムを取得
+    item_boxes = browser.find_elements(By.CSS_SELECTOR, '#item-grid > ul > li')
 
-    # 最大1件のページだけ取得する
-    for i, item_elem in enumerate(item_box):
+    for i, item_elem in enumerate(item_boxes):
         if i >= MAX_ITEMS:
             break
-        item_url_ls.append(item_elem.find_element(By.CSS_SELECTOR, 'a').get_attribute('href'))
+        # アイテムのリンクを取得
+        link_elements = item_elem.find_elements(By.CSS_SELECTOR, 'div > a')
+        if link_elements:
+            item_url = link_elements[0].get_attribute('href')
+            item_url_ls.append(item_url)
+            print(f"アイテム {i + 1} のリンクが見つかりました: {item_url}")
+            print("スクロールします。")
+            browser.execute_script("window.scrollBy(0, 100);")  # 1000px スクロール
+            time.sleep(0.05)  # スクロール後の待機
+        else:
+            print(f"アイテム {i + 1} にリンクが見つかりませんでした。")
+
+
+
+
+
+
 
 def get_data():
     # 商品情報の詳細を取得する
@@ -51,15 +66,15 @@ def get_data():
             # shopsのページの場合は、それより下のコードを実行する
 
             # 商品名
-            item_name = browser.find_element(By.CSS_SELECTOR, '.heading__a7d91561.page__a7d91561').text
+            item_name = browser.find_element(By.CSS_SELECTOR, '#product-info > section:nth-child(1) > div.mer-spacing-b-12 > div > div > h1').text
             #商品説明
-            item_ex = browser.find_element(By.CSS_SELECTOR, "pre[class='merText body__5616e150 primary__5616e150']").text
+            item_ex = browser.find_element(By.CSS_SELECTOR, "#product-info > section:nth-child(2) > div.merShowMore.mer-spacing-b-16 > div > pre").text
             #画像取得
-            src = browser.find_element(By.CSS_SELECTOR, "#main > article > div.sc-a6b7d8a7-2.WrJQp > section > div > div > div > div > div.sc-a6b7d8a7-2.ishIRQ > div.sc-e76b9217-0.vYGhZ > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img").get_attribute('src')
+            src = browser.find_element(By.CSS_SELECTOR, "#main > article > div.sc-1a095b48-2.dIrBJb > section > div > div > div > div > div.sc-1a095b48-2.bhLroK > div.sc-ab077df9-0.dvoZKC > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img").get_attribute('src')
             #値段取得
             item_price = browser.find_element(By.CSS_SELECTOR, "#product-info > section:nth-child(1) > section:nth-child(2) > div > div > span:nth-child(2)").text
             # 出品者の名前
-            owner_name = browser.find_element(By.CSS_SELECTOR, "p[class='merText body__5616e150 primary__5616e150 bold__5616e150']").text
+            owner_name = browser.find_element(By.CSS_SELECTOR, "#product-info > section:nth-child(4) > div.merListItem.withArrow__884ec505.hover__884ec505.sc-604b51a1-0.ildGFv.mer-spacing-b-4 > div.content__884ec505 > a > div > div > div.content__a9529387 > p").text
 
             data = {
                 '商品名': item_name,
@@ -73,15 +88,15 @@ def get_data():
             
         else:
             # 商品名
-            item_name = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > div.mer-spacing-b-12 > div > div > h1').text
+            item_name = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > div.mer-spacing-b-12 > div.merHeading.page__a7d91561.mer-spacing-b-2 > div > h1').text
             #商品説明
             item_ex = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(2) > div.merShowMore.mer-spacing-b-16 > div > pre').text
             #画像取得
-            src = browser.find_element(By.CSS_SELECTOR, '#main > article > div.sc-a6b7d8a7-2.WrJQp > section > div > div > div > div > div.sc-a6b7d8a7-2.ishIRQ > div > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img').get_attribute('src')
+            src = browser.find_element(By.CSS_SELECTOR, '#main > article > div.sc-1a095b48-2.sc-28317b1a-0.dIrBJb.dcHAay.mer-spacing-b-32 > section > div > div > div.sc-1a095b48-2.bhLroK > div > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img').get_attribute('src')
             #値段取得
-            item_price = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > section:nth-child(2) > div > div > span:nth-child(2)').text
+            item_price = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > section:nth-child(2) > div.sc-1a095b48-9.idbTzw.mer-spacing-b-16 > div > div > span:nth-child(2)').text
             # 出品者の名前
-            owner_name = browser.find_element(By.CSS_SELECTOR, 'p[class="merText body__5616e150 primary__5616e150 bold__5616e150"]').text
+            owner_name = browser.find_element(By.CSS_SELECTOR, "p[class='merText body__5616e150 primary__5616e150 bold__5616e150']").text
 
             data = {
                 '商品名': item_name,
